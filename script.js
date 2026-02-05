b636e33b4e0f2cae1853c5a71d882a6610e656c5
MUSTAFA HASSAN <mhwahid466@gmail.com>
2026-02-05 11:39:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,174 @@
+// alert("Hello World");
+// let name = "Mustafa";
+// console.log(name + age )
+// let message = "Assalamualaikum";             
+// alert(message);
+// console.log(message)
+// alert('welcome to java script')
+// let firstName = "Muhammad";
+// let lastName = "Usman";
+
+// alert(firstName + " " + lastName);
+// console.log(firstName+lastName)
+// let FirstName ='sufiyan'
+// let Age = '17year'
+// console.log(FirstName+Age)
+// alert(FirstName+Age)
+// let x = 10;
+// let y = 20;
+// let total = x + y;
+// alert(Total);    
+// let Total=20+10-5;
+// let marks=200/20
+// alert(marks)
+// let fee=12/1000;
+// alert(fee)
+//     alert('WelcomeToJavascript')
+//     alert('Mustafa Hassan')
+//     let countryname='pakistan'
+//     let country='Zindabad'
+//     alert(countryname+country)
+// let age='My age is 15'
+// alert(age)
+// console.log(total)
+// const student={
+// fullname:'mustafa hassan',
+// class:9,
+// school:'abcshcool',
+// roolnom:20,
+// age:15,
+// };
+// student ['fullname']='usman';
+// console.log(student ['fullname']);
+// student ['age'] + 1
+// console.log(student ['age']);
+// console.log(Total)
+// let TOtel=20/6;
+// console.log(TOtel)
+// let TOTal =10 %7
+// console.log(TOTal)
+// console.log(a)  
+// let b =20-9;
+// console.log(b)  
+// let c =4*6;
+// console.log(c)  
+// let d =15/3;
+// console.log(d)  
+// let e =17%4;
+// console.log(e)  
+// let A= 10;
+// a += 5;  
+// console.log(a) 
+// a -= 3;   
+// console.log(a)
+// a *= 2;  
+//  console.log(a)
+// a /= 4; 
+//  console.log(a)
+// a %= 4; 
+//  console.log(a)
+// let a =20;
+// let b =10;
+// console.log('a + b =', a + b)
+// console.log('a - b =', a - b)
+// console.log('a * b =', a * b)
+// console.log('a / b =', a / b)
+// let a =7;
+// let b =10;
+// a++;
+// console.log('a + b =',a)
+// for(let count = 1 ; count <=5 ;count++){
+//     console.log('mustafahassan')
+// } 
+// for (let i = 1 ; i<=100 ;i++){
+//     console.log('i =',i);
+//     console.log('mustafa hassan')
+
+// } 
+// console.log('mustafa hassan')
+  // alert(i)
+//   let i =1
+//   do{
+//     console.log('i =',i);
+//     i++
+//   }while ( i<=5) ;
+// let str ='mstafa hassan';
+// for(let i of str) {
+//     console.log('i =' ,i)
+// }
+// let str ='mstafa hassan sjdndhkvbj  jdvsgvbn m hws qnmwbhh, nmbgesfnm anmsb mn fqbj vacxejjknXM  jadsb nbnvawh bbnaw v jhfuyrbjUELWQJWJHGQRJHE H  QE JHQRJH QJHGRTNBQ JBBQ JB QRJH QETQ ERB EJHFGG AVNBC RHF WJWRRQJHJHRGEQLB GSmar gfjbembtjhqjc fajegugnmbevq wfr2huity5htrbqm 4ut urjh b getywccbgqcb r  ghjebqwqm';
+// size =0
+// for(let i of str) {
+//     console.log('i =' ,i)
+//     size++
+// }
+// console.log('String size =',size)
+// let student={
+//     name: 'mustafa hassan',
+//     age :15,
+//     roolnum:20,
+//     result:'passe'
+// }
+// for (let key in student){
+//     console.log('key =',key,'valule =', student[key])
+// }
+// for(let i = 0 ; i<=100; i++) {
+    // if(i%2 !== 0)
+    // if(i%1 === 0)
+    // console.log('1 + 1 =',i)
+
+    // alert(i)
+// }
+// let names =['mustafa','hassan', 'usman','ahmed'];
+// let marks =[25,50,75,100,125,];
+// console.log(marks)
+// console.log(names)
+// for(i = 0 ; i < 5 ; i++){
+//   console.log(names[i])
+// }
+// for ( let name of names){
+//   console.log(name)
+// }
+// let marks =[85,97,44,37,76,60];
+// let sum =0;
+// for(let valu of marks){
+//   sum+=valu;
+// }
+// console.log(sum)
+//   let avg = sum / marks.length;
+// console.log( `avrage marks =${avg}` );
+// let items =[250,645,300,900,50]
+// console.log(items)
+// let i =0;
+// for(let valu of items){
+//   console.log(valu)
+//   let offar =valu/10;
+//   items[i]=items[i ]-offar
+//   console.log(`valu after valu${items[i]}`)
+//   i++
+// }
+// let fooditems =['biryani','burgar','chips']
+// fooditems.push('rool','kabab')
+// console.log(fooditems)
+//  fooditems =['biryani','burgar','chips']
+// console.log(fooditems)
+// let deleteditems = fooditems.pop();
+// console.log(deleteditems)
+// fooditems =['biryani','burgar','chips']
+// console.log(fooditems);
+// console.log(fooditems.toString());
+//  let names =['mustafa','hassan','ahmed']
+//  let firstName=['usman','kashan']
+//  let juindnames=names.concat(firstName);
+//  let names =['mustafa','hassan','ahmed']
+// console.log(names)
+// console.log(names.slice(2))
+//  let number =[1,2,3,4,5,6,7,8,9,10]
+//  console.log(number); 
+//  number.splice(2,0,101);
+// let compines =['mictrosoft','google','pcb','uber','in draive'];
+// console.log(compines)
+// compines.push('feacbook')
+// compines.splice(5,0.,'feacbook')
+// compines.shift()
+// compines.splice(3,1,'ola')
