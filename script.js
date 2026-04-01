305f432503170791d52f3707f0f0d2d6f1828f65
MUSTAFA HASSAN <mhwahid466@gmail.com>
2026-04-01 18:13:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,244 +1,150 @@
-// alert("Hello World");
-// let name = "Mustafa";
-// console.log(name + age )
-// let message = "Assalamualaikum";             
-// alert(message);
-// console.log(message)
-// alert('welcome to java script')
-// let firstName = "Muhammad";
-// let lastName = "Usman";
+alert("Hello World");
 
-// alert(firstName + " " + lastName);
-// console.log(firstName+lastName)
-// let FirstName ='sufiyan'
-// let Age = '17year'
-// console.log(FirstName+Age)
-// alert(FirstName+Age)
-// let x = 10;
-// let y = 20;
-// let total = x + y;
-// alert(Total);    
-// let Total=20+10-5;
-// let marks=200/20
-// alert(marks)
-// let fee=12/1000;
-// alert(fee)
-//     alert('WelcomeToJavascript')
-//     alert('Mustafa Hassan')
-//     let countryname='pakistan'
-//     let country='Zindabad'
-//     alert(countryname+country)
-// let age='My age is 15'
-// alert(age)
-// console.log(total)
-// const student={
-// fullname:'mustafa hassan',
-// class:9,
-// school:'abcshcool',
-// roolnom:20,
-// age:15,
-// };
-// student ['fullname']='usman';
-// console.log(student ['fullname']);
-// student ['age'] + 1
-// console.log(student ['age']);
-// console.log(Total)
-// let TOtel=20/6;
-// console.log(TOtel)
-// let TOTal =10 %7
-// console.log(TOTal)
-// console.log(a)  
-// let b =20-9;
-// console.log(b)  
-// let c =4*6;
-// console.log(c)  
-// let d =15/3;
-// console.log(d)  
-// let e =17%4;
-// console.log(e)  
-// let A= 10;
-// a += 5;  
-// console.log(a) 
-// a -= 3;   
-// console.log(a)
-// a *= 2;  
-//  console.log(a)
-// a /= 4; 
-//  console.log(a)
-// a %= 4; 
-//  console.log(a)
-// let a =20;
-// let b =10;
-// console.log('a + b =', a + b)
-// console.log('a - b =', a - b)
-// console.log('a * b =', a * b)
-// console.log('a / b =', a / b)
-// let a =7;
-// let b =10;
-// a++;
-// console.log('a + b =',a)
-// for(let count = 1 ; count <=5 ;count++){
-//     console.log('mustafahassan')
-// } 
-// for (let i = 1 ; i<=100 ;i++){
-//     console.log('i =',i);
-//     console.log('mustafa hassan')
+let name = "Mustafa";
+console.log(name);
 
-// } 
-// console.log('mustafa hassan')
-  // alert(i)
-//   let i =1
-//   do{
-//     console.log('i =',i);
-//     i++
-//   }while ( i<=5) ;
-// let str ='mstafa hassan';
-// for(let i of str) {
-//     console.log('i =' ,i)
-// }
-// let str ='mstafa hassan sjdndhkvbj  jdvsgvbn m hws qnmwbhh, nmbgesfnm anmsb mn fqbj vacxejjknXM  jadsb nbnvawh bbnaw v jhfuyrbjUELWQJWJHGQRJHE H  QE JHQRJH QJHGRTNBQ JBBQ JB QRJH QETQ ERB EJHFGG AVNBC RHF WJWRRQJHJHRGEQLB GSmar gfjbembtjhqjc fajegugnmbevq wfr2huity5htrbqm 4ut urjh b getywccbgqcb r  ghjebqwqm';
-// size =0
-// for(let i of str) {
-//     console.log('i =' ,i)
-//     size++
-// }
-// console.log('String size =',size)
-// let student={
-//     name: 'mustafa hassan',
-//     age :15,
-//     roolnum:20,
-//     result:'passe'
-// }
-// for (let key in student){
-//     console.log('key =',key,'valule =', student[key])
-// }
-// for(let i = 0 ; i<=100; i++) {
-    // if(i%2 !== 0)
-    // if(i%1 === 0)
-    // console.log('1 + 1 =',i)
+let message = "Assalamualaikum";
+alert(message);
+console.log(message);
 
-    // alert(i)
-// }
-// let names =['mustafa','hassan', 'usman','ahmed'];
-// let marks =[25,50,75,100,125,];
-// console.log(marks)
-// console.log(names)
-// for(i = 0 ; i < 5 ; i++){
-//   console.log(names[i])
-// }
-// for ( let name of names){
-//   console.log(name)
-// }
-// let marks =[85,97,44,37,76,60];
-// let sum =0;
-// for(let valu of marks){
-//   sum+=valu;
-// }
-// console.log(sum)
-//   let avg = sum / marks.length;
-// console.log( `avrage marks =${avg}` );
-// let items =[250,645,300,900,50]
-// console.log(items)
-// let i =0;
-// for(let valu of items){
-//   console.log(valu)
-//   let offar =valu/10;
-//   items[i]=items[i ]-offar
-//   console.log(`valu after valu${items[i]}`)
-//   i++
-// }
-// let fooditems =['biryani','burgar','chips']
-// fooditems.push('rool','kabab')
-// console.log(fooditems)
-//  fooditems =['biryani','burgar','chips']
-// console.log(fooditems)
-// let deleteditems = fooditems.pop();
-// console.log(deleteditems)
-// fooditems =['biryani','burgar','chips']
-// console.log(fooditems);
-// console.log(fooditems.toString());
-//  let names =['mustafa','hassan','ahmed']
-//  let firstName=['usman','kashan']
-//  let juindnames=names.concat(firstName);
-//  let names =['mustafa','hassan','ahmed']
-// console.log(names)
-// console.log(names.slice(2))
-//  let number =[1,2,3,4,5,6,7,8,9,10]
-//  console.log(number); 
-//  number.splice(2,0,101);
-// let compines =['mictrosoft','google','pcb','uber','in draive'];
-// console.log(compines)
-// compines.push('feacbook')
-// compines.splice(5,0.,'feacbook')
-// compines.shift()
+alert("Welcome to JavaScript");
 
-// compines.splice(3,1,'ola')/ console.log(fullname)
-// console.log(`my name\t is\t  ${fullname}`);
-// console.log(`my name is  ${fullname}`);
-// prompt('enter your name')
-// console.log('hello \n world') 
-// let str ='              mustafa hassan        '; 
-// console.log(str.length) 
-// console.log(str.toUpperCase()) 
-// console.log(str.toLowerCase()) 
-// console.log(str.trim()) 
-// let str =' mustafa wahid'; 
-// console.log(str)
-// let joinstr =str + ' is a good boy';
-// console.log(joinstr)
-// console.log(str.replace('mustafa','usman','ahmed'))
-// console.log(str.charAt(10))
-// let fullname = prompt('Enter your name with out space')
-// let joinstr =fullname +'@gmail.com'+ '\n' + fullname.length;
-// console.log(joinstr)
-// let age =15;
-// if(age >= 18){
-//      console.log('you are eligible for vote')
-// }
-// if(age < 18){
-//     console.log('you are not eligible for vote')
-// }
-// let mood ='happy';
-// if(mood === 'happy'){
-//     console.log('i am in good mood')
-// }
-// let date = 2-12-2026;
-// if(date === 3-12-2026){
-//   console.log('date is correct');
-// } else {
-//   console.log('date is not correct');
-// }
-// let mood = 'white';
-// let color = '';
-// if(mood === 'black'){
-//   color = 'dark';
-//   } else if(mood === 'white'){
-//     color = 'light';
-//   } else if(mood === 'red'){
-//     color = 'danger';
-//   } else {
-//     color = 'unknown';
-//   }
-//   console.log(color)
-// let input = prompt('Enter your name');
-// let output = 'Hello ' + input;
-// alert(output)
-// for(let i = 1 ; i <=10 ; i++){
-//     // console.log('i =',i);
-    // console.log(`10 x  ${i} =`,10*i);
-// }
-// let age = prompt('Enter your age');
-// console.log('age =',age);
-// for(let i =0  ; i <= 100 ; i++){
-//     if(i%2 === 0){
-//         console.log('even number =',i);
-//     } else {
-//         console.log('odd number =',i);
-//     }
-//   }
-// let num1 = prompt('Enter first number');
-// let num2 = prompt('Enter second number');
-// console.log(`sum =${num1 + num2}`)
-// console.log(`subtrict = ${num1 - num2}`)
-// console.log(`multiply = ${num1 * num2}`)
-// console.log(`divde = ${num1 / num2}`)
+let firstName = "Muhammad";
+let lastName = "Usman";
+
+alert(firstName + " " + lastName);
+console.log(firstName + " " + lastName);
+
+let FirstName = "Sufiyan";
+let Age = "17 year";
+console.log(FirstName + " " + Age);
+alert(FirstName + " " + Age);
+
+let x = 10;
+let y = 20;
+let total = x + y;
+alert(total);
+
+let Total = 20 + 10 - 5;
+let marks = 200 / 20;
+alert(marks);
+
+let fee = 12 / 1000;
+alert(fee);
+
+alert("Welcome To Javascript");
+alert("Mustafa Hassan");
+
+let countryname = "Pakistan ";
+let country = "Zindabad";
+alert(countryname + country);
+
+let ageText = "My age is 15";
+alert(ageText);
+
+console.log(total);
+
+const student = {
+  fullname: "mustafa hassan",
+  class: 9,
+  school: "abc school",
+  rollno: 20,
+  age: 15,
+};
+
+student["fullname"] = "usman";
+console.log(student["fullname"]);
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
+
+console.log(Total);
+
+let TOtel = 20 / 6;
+console.log(TOtel);
+
+let TOTal = 10 % 7;
+console.log(TOTal);
+
+let b = 20 - 9;
+console.log(b);
+
+let c = 4 * 6;
+console.log(c);
+
+let d = 15 / 3;
+console.log(d);
+
+let e = 17 % 4;
+console.log(e);
+
+let A = 10;
+A += 5;
+console.log(A);
+
+A -= 3;
+console.log(A);
+
+A *= 2;
+console.log(A);
+
+A /= 4;
+console.log(A);
+
+A %= 4;
+console.log(A);
+
+let s = 20;
+let bb = 10;
+
+console.log("a + b =", s + bb);
+console.log("a - b =", s - bb);
+console.log("a * b =", s * bb);
+console.log("a / b =", s / bb);
+
+let a = 7;
+a++;
+console.log("a =", a);
+
+// loop
+for (let count = 1; count <= 5; count++) {
+  console.log("mustafa hassan");
+}
+
+// array
+let names = ["mustafa", "hassan", "usman", "ahmed"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+// functions
+function myFunction() {
+  console.log("welcome to javascript");
+}
+myFunction();
+
+function sum(a, b) {
+  return a + b;
+}
+let val = sum(10, 20);
+console.log(val);
+
+// vowels
+function countVowels(str) {
+  let count = 0;
+  for (let char of str) {
+    if ("aeiouAEIOU".includes(char)) {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+countVowels("mustafa hassan");
+
+// foreach
+let nums = [1, 2, 3, 4, 5];
+nums.forEach((num) => {
+  console.log(num * num);
+});
